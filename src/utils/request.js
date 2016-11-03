7a16d9379ce62d4aabcf6f240d0f7f4721151608
T. Brandon Ashley <tb@tba.sh>
2016-11-03 02:03:25 -0400

    add general request fn w/fancy arrows

---
+++ src/utils/request.js
@@ -0,0 +1,23 @@
+const request = (url, options) => {
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(parseJSON)
+    .then((data) => ({ data }))
+    .catch((err) => ({ err }));
+}
+
+const parseJSON = (response) => {
+  return response.json();
+}
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+export default request;
